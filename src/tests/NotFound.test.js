92e99e355cf35b1336445bf9fafd9c56e4e01bf6
Jamile <milli.santana2017@gmail.com>
2022-05-17 17:38:46 -0300

    adiciona requisito 4, chorando por ter ficado em recuperaçao :(

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import renderWithRouter from '../RenderWithRouter';
+import { NotFound } from '../components';
+
+describe('Aplique os testes necessários ao componente NotFound', () => {
+  test('A página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const pagina = screen.getByRole('heading', { name: /Page requested not found/ });
+    expect(pagina).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const imagem = screen.getByRole(
+      'img', { name: /pikachu crying because the page requested was not found/i },
+    );
+    expect(imagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
