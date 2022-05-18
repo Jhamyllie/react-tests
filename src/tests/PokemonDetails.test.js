c72bd2afa36195fe2d0ed053fc78ffb6cc0b5beb
Jamile <milli.santana2017@gmail.com>
2022-05-18 18:44:42 -0300

    adiciona requisito 7 para a gloria do senhor

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../RenderWithRouter';
+import App from '../App';
+
+describe('Aplique os teste necessários ao  PokemonDetails', () => {
+  test('A página deve conter um texto <name> Details', () => {
+    renderWithRouter(<App />);
+    const link = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(link);
+    const nomeDoPokemon = screen.getByRole('heading', { name: /pikachu details/i });
+    expect(nomeDoPokemon).toBeInTheDocument();
+  });
+});
