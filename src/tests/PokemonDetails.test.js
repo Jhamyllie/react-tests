b0668b6d08fe48389e865f964ae22fea293f1707
Jamile <milli.santana2017@gmail.com>
2022-05-20 12:37:52 -0300

    adiciona requisito 7, finalmente saindo da recuperaçao, para  a gloria do senhor

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,11 +5,78 @@ import renderWithRouter from '../RenderWithRouter';
 import App from '../App';
 
 describe('Aplique os teste necessários ao  PokemonDetails', () => {
-  test('A página deve conter um texto <name> Details', () => {
+  test('Não deve existir o link de navegação '
+  + 'para os detalhes do pokémon selecionado', () => {
+    renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /more details/i });
+
+    userEvent.click(link);
+
+    const nomeDoPokemon = screen.getByRole('heading', { name: /pikachu details/i });
+
+    expect(nomeDoPokemon).toBeInTheDocument();
+  });
+
+  test('A seção de detalhes deve conter um heading h2 com o texto Summary', () => {
+    renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /more details/i });
+
+    userEvent.click(link);
+
+    const sumary = screen.getByRole('heading', { level: 2, name: 'Summary' });
+
+    expect(sumary).toBeInTheDocument();
+  });
+
+  test('A seção de detalhes deve conter um parágrafo com o resumo do pokémon'
+  + 'especificado sendo visualizado', () => {
+    renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /more details/i });
+
+    userEvent.click(link);
+
+    const pokemonFavorito = screen.getByText(/pokémon favoritado\?/i);
+
+    const pokemonResumo = screen
+      .getByText(/this intelligent pokémon roasts hard berries with electricity/i);
+
+    expect(pokemonFavorito).toBeInTheDocument();
+
+    expect(pokemonResumo).toBeInTheDocument();
+  });
+
+  test('Na seção de detalhes deverá existir um heading'
+  + ' h2 com o texto Game Locations of <name>', () => {
     renderWithRouter(<App />);
     const link = screen.getByRole('link', { name: /more details/i });
     userEvent.click(link);
-    const nomeDoPokemon = screen.getByRole('heading', { name: /pikachu details/i });
-    expect(nomeDoPokemon).not.toBeInTheDocument();
+    // const name = pikachu;
+    const detalhes = screen.getByRole('heading',
+      { name: /game locations of pikachu/i });
+    expect(detalhes).toBeInTheDocument();
+  });
+
+  test('Devem ser exibidos o nome da localização e uma imagem'
+  + 'do mapa em cada localização', () => {
+    renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(link);
+
+    const imgLocalizacao = screen.getAllByAltText(/Pikachu location/i);
+    expect(imgLocalizacao[0]).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+
+    expect(imgLocalizacao[1]).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
   });
 });
+
+// <h2>{ `Game Locations of ${name}` }</h2>
+//  <h2>{ `Summary` }</h2> feito
+// <img src={ `${map}` } alt={ `${name} location` } />
+// { `Pokémon favoritado?` }
+// <p>{ `${summary}` }</p>
